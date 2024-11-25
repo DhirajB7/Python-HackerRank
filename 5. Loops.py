3db28abbf5b15926c58edbca454ab853dfa0b6ca
Dhiraj <dhiraj.basavaraju@gmail.com>
2024-11-26 00:54:26 +0530

    first commit

---
+++ 5. Loops.py
@@ -0,0 +1,5 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = [x*x for x in range(n)]
+    for x in a :
+        print(x)
---
+++ 7. Print Function.py
@@ -0,0 +1,4 @@
+if __name__ == '__main__':
+    n = int(input())
+    a = [str(x) for x in range(1,n+1)]
+    print("".join(a))
